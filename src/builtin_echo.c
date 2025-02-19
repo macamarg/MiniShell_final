8463b0aff57c5ac16ff4399c1cced50d5732d13a
Joana Maria Rocha Fernandes Oliveira E Silva <jrocha-f@c2r6s2.42lisboa.com>
2025-02-19 12:02:19 +0000

    norm

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-f <jrocha-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:19:29 by jrocha-f          #+#    #+#             */
-/*   Updated: 2024/12/18 14:12:36 by jrocha-f         ###   ########.fr       */
+/*   Updated: 2025/02/19 12:00:45 by jrocha-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,45 @@
 
 #include "../minishell.h"
 
-static bool	is_n_flag (char *cmd)
+static void	echo_text(t_command *cmd, int i)
 {
-	int i;
+	while (cmd->cmd[i])
+	{
+		ft_putstr_fd(cmd->cmd[i], cmd->fd_out);
+		if (cmd->cmd[i + 1])
+			ft_putchar_fd(' ', cmd->fd_out);
+		i++;
+	}
+}
+
+static bool	is_n_flag(char *cmd)
+{
+	int	i;
 
 	i = 1;
 	if (!ft_strncmp(cmd, "-n", 3))
-		return(1);
+		return (1);
 	else if (!ft_strncmp(cmd, "-n", 2))
 	{
 		while (cmd[++i])
 		{
 			if (cmd[i] != 'n')
-				return(0);
+				return (0);
 		}
-		return(1);
+		return (1);
 	}
 	else
-		return(0);
+		return (0);
 }
 
 int	ft_echo(t_command *cmd)
 {
-	int	i;
-	bool flag;
+	int		i;
+	bool	flag;
 
 	i = 1;
 	flag = false;
-	while(cmd->cmd[i] && is_n_flag(cmd->cmd[i]))
+	while (cmd->cmd[i] && is_n_flag(cmd->cmd[i]))
 	{
 		flag = true;
 		i++;
@@ -57,14 +68,8 @@ int	ft_echo(t_command *cmd)
 			ft_putchar_fd('\n', cmd->fd_out);
 		return (0);
 	}
-	while (cmd->cmd[i])
-	{
-		ft_putstr_fd (cmd->cmd[i], cmd->fd_out);
-		if (cmd->cmd[i + 1])
-			ft_putchar_fd(' ', cmd->fd_out);
-		i++;
-	}
+	echo_text(cmd, i);
 	if (!flag)
 		ft_putchar_fd('\n', cmd->fd_out);
-	return(0);
+	return (0);
 }
