ec15b81b44e3e7a82682c30b67279ada31a00b80
Joana Maria Rocha Fernandes Oliveira E Silva <jrocha-f@c2r3s3.42lisboa.com>
2025-03-11 10:20:54 +0000

    change error handling env

--- src/builtin_env.c
+++ src/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: jrocha-f <jrocha-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:09:33 by marvin            #+#    #+#             */
-/*   Updated: 2025/02/19 12:05:37 by jrocha-f         ###   ########.fr       */
+/*   Updated: 2025/03/11 10:17:14 by jrocha-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,8 @@ int	ft_env(t_command *cmd, t_minishell *master)
 	int	i;
 
 	i = -1;
-	if (cmd->cmd[1])
-	{
-		ft_putstr_fd("env:", 2);
-		ft_putstr_fd(cmd->cmd[1], 2);
-		ft_putstr_fd("No such file or directory \n", 2);
-		return (127);
-	}
+	if(!master->env)
+		return (1);
 	while (master->env[++i])
 		printf("%s\n", master->env[i]);
 	return (0);
